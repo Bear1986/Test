ce085db53e55698a4328c68000bd461479e27b66
Arthur Beckett <art.b.beckett@gmail.com>
2021-11-12 12:08:06 +0000

    1st steps into Java

---
+++ HelloWorld/src/hello.java
@@ -0,0 +1,15 @@
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Art");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber =6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+        System.out.println();
+    }
+}
